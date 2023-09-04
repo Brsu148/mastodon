24d72b31eb65bb358bb61734820e72631747bb87
Brsu <brsu148@gmail.com>
2023-09-04 23:41:49 +0800

    Update status_length_validator.rb

--- app/validators/status_length_validator.rb
+++ app/validators/status_length_validator.rb
@@ -32,11 +32,11 @@ class StatusLengthValidator < ActiveModel::Validator
     # will be correctly parsed during formatting, we go through full
     # entity extraction
 
-    entities = Extractor.remove_overlapping_entities(
+  entities = Extractor.remove_overlapping_entities(
     Extractor.extract_urls_with_indices(str, extract_url_without_protocol: false) +
-    Extractor.extract_mentions_or_lists_with_indices(str)
-    )
-    
+      Extractor.extract_mentions_or_lists_with_indices(str)
+  )
+
     rewrite_entities(str, entities) do |entity|
       if entity[:url]
         URL_PLACEHOLDER
@@ -48,7 +48,7 @@ class StatusLengthValidator < ActiveModel::Validator
 
   def rewrite_entities(str, entities)
     entities.sort_by! { |entity| entity[:indices].first }
-    result = +''.dup
+    result = + ''.dup
 
     last_index = entities.reduce(0) do |index, entity|
       result << str[index...entity[:indices].first]
