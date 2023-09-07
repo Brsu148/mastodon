28b21f7f1a794a13527c2597a534e8ca78efd50d
Brsu <brsu148@gmail.com>
2023-09-07 12:57:08 +0800

    progress.total = 2000

--- lib/mastodon/search_cli.rb
+++ lib/mastodon/search_cli.rb
@@ -64,7 +64,7 @@ module Mastodon
       end
 
       progress.title = 'Estimating workload '
-      progress.total = indices.sum { |index| importers[index].estimate! }
+      progress.total = 2000
 
       reset_connection_pools!
 
